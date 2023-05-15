a3bb0a5f96008a2e7e69c6d028973a20635eaa49
GalaDriel <g_ala@naver.com>
2023-05-15 21:03:55 +0900

    Add: Components - CommmonHeader.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
+    <CommonHeader/>
     <div>
         <RouterView/>
     </div>
 </template>
 
 <script setup lang="ts">
-
+import CommonHeader from '@/components/CommonHeader.vue';
 </script>
 
 <style lang="scss">
---
+++ src/components/CommonHeader.vue
@@ -0,0 +1,47 @@
+<template>
+    <header>
+        <div class="headerWrap">
+            <h1>
+                <a href="#home">Moon<span>。</span></a>
+            </h1>
+            <div>
+                <nav class="nav">
+                    <ul id="menu">
+                        <li data-menuanchor="home">
+                            <a href="#home"><span>Home</span></a>
+                        </li>
+                        <li data-menuanchor="about">
+                            <a href="#about"><span>About</span></a>
+                        </li>
+                        <li data-menuanchor="projects">
+                            <a href="#projects"><span>Projects</span></a>
+                        </li>
+                        <li data-menuanchor="contact">
+                            <a href="#contact"><span>Contact</span></a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- <button
+                    :class="isDark"
+                    @click="onClickDark"
+                    class="dark"
+                ></button>
+                <a
+                    href="javascript:void(0)"
+                    class="wholeBtn"
+                    @click="onToggleActive"
+                >
+                    <span :class="{ active: isActive }">&nbsp;</span>
+                </a> -->
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+@import '@/assets/components/CommonHeader.scss';
+</style>
